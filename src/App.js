9c6727f0f98562e107df3747da93596279c84930
sai ramam <dsairaman2005@gmail.com>
2025-02-24 08:42:06 +0530

    Project Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react"
 import {
   BrowserRouter as Router,
   Route,
@@ -38,6 +39,7 @@ const App = () => {
         />
       
       </Routes>
+      <Analytics />
     </Router>
   );
 };
